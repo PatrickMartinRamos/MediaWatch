35777f3845e3f12f0468c5b7cbfcdcd530fd7e74
PatrickRamos <122716128+PatrickMartinRamos@users.noreply.github.com>
2024-03-15 11:48:23 +0800

    ..

--- Assets/modelScript.cs
+++ Assets/modelScript.cs
@@ -6,30 +6,42 @@ using UnityEngine;
 public class modelScript : MonoBehaviour
 {
     public GameObject phoneModel;
+    [Header("start rotate")]
+    public bool startTransition1 = false;
+    [Header("switch rotate")]
+    public bool startTransition2 = false;
 
-    public bool startRotate = false;
-    public Vector3 startLocation;
-
-    public bool switchPhone = false;
+    [Header("cameras")]
+    public GameObject vCam_1;
 
     private void Start()
     {
-        phoneModel.transform.localPosition = startLocation;
+        vCam_1.transform.position = new Vector3(0, 0, 0);
     }
 
     private void Update()
     {
-        rotatePhoneModel();
+        transition_1(); 
+        transition_2();
     }
 
-    void rotatePhoneModel()
+    void transition_1()
     {
-        if (startRotate)
+        if (startTransition1)
         {
             phoneModel.transform.DOLocalRotate(new Vector3(0f, 360f, 0f), 2f, RotateMode.FastBeyond360)
-                .SetLoops(1, LoopType.Restart);
+                .SetLoops(1);
 
             phoneModel.transform.DOMoveX(0f, 1f).SetEase(Ease.Linear);
+            vCam_1.transform.DOMoveZ(-900f, 1f).SetEase(Ease.Linear);
+
+        }
+    }
+    void transition_2()
+    {
+        if(startTransition2)
+        {
+            phoneModel.transform.DOMoveX(-900, 2f).SetEase(Ease.Linear);
         }
     }
 }
