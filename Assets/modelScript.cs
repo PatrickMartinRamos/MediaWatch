15d26da7ce1f228abbf9c99168469f6d886f127a
PatrickRamos <122716128+PatrickMartinRamos@users.noreply.github.com>
2024-03-15 10:16:25 +0800

    ....

--- Assets/modelScript.cs
+++ Assets/modelScript.cs
@@ -1,18 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
 public class modelScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject phoneModel;
+
+    public bool startRotate = false;
+    public Vector3 startLocation;
+
+    public bool switchPhone = false;
+
+    private void Start()
     {
-        
+        phoneModel.transform.localPosition = startLocation;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        rotatePhoneModel();
+    }
+
+    void rotatePhoneModel()
+    {
+        if (startRotate)
+        {
+            phoneModel.transform.DOLocalRotate(new Vector3(0f, 360f, 0f), 1f, RotateMode.FastBeyond360)
+                .SetLoops(1, LoopType.Restart);
+
+            phoneModel.transform.DOMoveX(0f, 1f).SetEase(Ease.Linear);
+        }
     }
 }
